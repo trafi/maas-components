e1fdb4ffa8a267f37d480ffe9d9b0d987351ad4a
Justas Medeisis <justas@trafi.com>
2020-09-11 10:24:53 +0300

    Demo route search app.

--- app/src/main/java/com/trafi/example/MainActivity.kt
+++ app/src/main/java/com/trafi/example/MainActivity.kt
@@ -2,37 +2,12 @@ package com.trafi.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
 import com.trafi.example.ui.MaasTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MaasTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MaasTheme {
-        Greeting("Android")
+        setContent { MaasTheme { DemoApp() } }
     }
 }
