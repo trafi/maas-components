2954acc64895370d37d267ab0f7016a11e376721
Justas Medeisis <justas@trafi.com>
2020-09-18 20:46:21 +0300

    Dark theme support in demo app.

--- android/app/src/main/java/com/trafi/example/ui/Theme.kt
+++ android/app/src/main/java/com/trafi/example/ui/Theme.kt
@@ -3,6 +3,8 @@ package com.trafi.example.ui
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import com.trafi.ui.theme.MaasTheme
+import com.trafi.ui.theme.darkColors
+import com.trafi.ui.theme.lightColors
 
 @Composable
 fun DemoMaasTheme(
@@ -10,6 +12,7 @@ fun DemoMaasTheme(
     content: @Composable () -> Unit
 ) {
     MaasTheme(
+        colors = if (darkTheme) MaasTheme.darkColors() else MaasTheme.lightColors(),
         typography = typography,
         content = content
     )
