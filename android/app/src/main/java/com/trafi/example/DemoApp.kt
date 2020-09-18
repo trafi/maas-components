e4a97b6b45c803085c9fd6094dc89f04324586cc
Justas Medeisis <justas@trafi.com>
2020-09-18 23:22:25 +0300

    Implement navigation between onboarding and routing screens in the demo app.

---
+++ android/app/src/main/java/com/trafi/example/DemoApp.kt
@@ -0,0 +1,35 @@
+package com.trafi.example
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import com.trafi.example.ui.DemoMaasTheme
+import com.trafi.example.util.NavigationViewModel
+import com.trafi.example.util.Screen
+import com.trafi.ui.theme.MaasTheme
+
+@Composable
+fun DemoApp(navigationViewModel: NavigationViewModel) {
+    DemoMaasTheme {
+        AppContent(navigationViewModel)
+    }
+}
+
+@Composable
+private fun AppContent(
+    navigationViewModel: NavigationViewModel
+) {
+    Crossfade(navigationViewModel.currentScreen) { screen ->
+        Surface(
+            color = MaasTheme.colors.background,
+            contentColor = MaasTheme.colors.onBackground
+        ) {
+            when (screen) {
+                is Screen.Onboarding -> OnboardingScreen(
+                    onComplete = { navigationViewModel.navigateTo(Screen.Routes) },
+                )
+                is Screen.Routes -> RoutesScreen()
+            }
+        }
+    }
+}
--- android/app/src/main/java/com/trafi/example/MainActivity.kt
+++ android/app/src/main/java/com/trafi/example/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.trafi.example
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.setContent
-import com.trafi.example.ui.DemoMaasTheme
+import com.trafi.example.util.NavigationViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val navigationViewModel by viewModels<NavigationViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DemoMaasTheme {
-                RoutesScreen()
-            }
+        setContent { DemoApp(navigationViewModel) }
+    }
+
+    override fun onBackPressed() {
+        if (!navigationViewModel.onBack()) {
+            super.onBackPressed()
         }
     }
 }
