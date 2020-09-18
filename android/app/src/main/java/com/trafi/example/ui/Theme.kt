85c90abe4bc7b54daed07bd8cb6440e9c517538a
Justas Medeisis <justas@trafi.com>
2020-09-18 22:38:27 +0300

    Demo app theme adjusts global margin based on screen width.

--- android/app/src/main/java/com/trafi/example/ui/Theme.kt
+++ android/app/src/main/java/com/trafi/example/ui/Theme.kt
@@ -2,17 +2,28 @@ package com.trafi.example.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ConfigurationAmbient
+import com.trafi.ui.theme.MaasSpacing
 import com.trafi.ui.theme.MaasTheme
+import com.trafi.ui.theme.Spacing
 import com.trafi.ui.theme.darkColors
 import com.trafi.ui.theme.lightColors
 
 @Composable
 fun DemoMaasTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    narrowScreen: Boolean = isScreenWidthNarrow(),
     content: @Composable () -> Unit
 ) {
     MaasTheme(
         colors = if (darkTheme) MaasTheme.darkColors() else MaasTheme.lightColors(),
+        spacing = MaasSpacing(globalMargin = if (narrowScreen) Spacing.md else Spacing.xl),
         content = content
     )
+}
+
+@Composable
+fun isScreenWidthNarrow(): Boolean {
+    val configuration = ConfigurationAmbient.current
+    return configuration.screenWidthDp <= 280
 }
