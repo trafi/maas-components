0f82dbfca1f3f4676037e5f460d2bcfd2e9cdbf6
Domas Nutautas <domas@trafi.com>
2020-10-02 16:15:44 +0300

    Optional result for the demo

--- ios/Sources/MaaS/UI/RoutesResultView.swift
+++ ios/Sources/MaaS/UI/RoutesResultView.swift
@@ -3,15 +3,21 @@ import shared
 
 public struct RoutesResultView: View {
 
-    public init(_ result: RoutesResult) {
+    public init(_ result: RoutesResult?) {
         self.result = result
     }
 
-    let result: RoutesResult
+    let result: RoutesResult?
 
     public var body: some View {
-        List(result.routes, id: \.id) {
-            RouteView(route: $0)
+        Group {
+            if result != nil {
+                List(result!.routes, id: \.id) {
+                    RouteView(route: $0)
+                }
+            } else {
+                Text("Searching...")
+            }
         }
     }
 }
