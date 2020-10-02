474e56248f5193dfee5eb2cf9a5afbde22a5a0be
Justas Medeisis <justas@trafi.com>
2020-10-02 14:35:37 +0300

    Implement CommonEnumSerializer.

--- common/shared/src/commonMain/kotlin/EnumDescriptor.kt
+++ common/shared/src/commonMain/kotlin/EnumDescriptor.kt
@@ -4,75 +4,47 @@
 
 package kotlinx.serialization.internal
 
-import kotlinx.serialization.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.SerialKind
+import kotlinx.serialization.descriptors.StructureKind
+import kotlinx.serialization.descriptors.buildSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlin.reflect.KClass
 
-//public class EnumDescriptor(override val name: String, private val choices: Array<String>) : SerialClassDescImpl(name) {
-//    override val kind: SerialKind = UnionKind.ENUM_KIND
-//
-//    init {
-//        choices.forEach { addElement(it) }
-//    }
-//
-//    override fun getElementDescriptor(index: Int): SerialDescriptor {
-//        return this
-//    }
-//
-//    override fun equals(other: Any?): Boolean {
-//        if (this === other) return true
-//        if (other == null || this::class != other::class) return false
-//
-//        other as EnumDescriptor
-//
-//        if (name != other.name) return false
-//        if (!choices.contentEquals(other.choices)) return false
-//
-//        return true
-//    }
-//
-//    override fun hashCode(): Int {
-//        var result = super.hashCode()
-//        result = 31 * result + name.hashCode()
-//        result = 31 * result + choices.contentHashCode()
-//        return result
-//    }
-//}
-
-open class CommonEnumSerializer<T>(val serialName: String, val choices: Array<T>, choicesNames: Array<String>) :
+@OptIn(ExperimentalSerializationApi::class)
+open class CommonEnumSerializer<T>(
+    val serialName: String,
+    val values: Array<T>,
+    val choicesNames: Array<String>
+) :
     KSerializer<T> {
-//    override val descriptor: EnumDescriptor = EnumDescriptor(serialName, choicesNames)
-//
-//    final override fun serialize(encoder: Encoder, obj: T) {
-//        val index = choices.indexOf(obj)
-//            .also { check(it != -1) { "$obj is not a valid enum $serialName, choices are $choices" } }
-//        encoder.encodeEnum(descriptor, index)
-//    }
-//
-//    final override fun deserialize(decoder: Decoder): T {
-//        val index = decoder.decodeEnum(descriptor)
-//        check(index in choices.indices)
-//            { "$index is not among valid $serialName choices, choices size is ${choices.size}" }
-//        return choices[index]
-//    }
 
     override fun deserialize(decoder: Decoder): T {
-        TODO("Not yet implemented")
+        val index = decoder.decodeEnum(descriptor)
+        check(index in values.indices) {
+            "$index is not among valid $${descriptor.serialName} enum values, values size is ${values.size}"
+        }
+        return values[index]
     }
 
     override fun serialize(encoder: Encoder, value: T) {
-        TODO("Not yet implemented")
+        val index = values.indexOf(value)
+        check(index != -1) {
+            "$value is not a valid enum ${descriptor.serialName}, must be one of ${values.contentToString()}"
+        }
+        encoder.encodeEnum(descriptor, index)
     }
 
+    @OptIn(InternalSerializationApi::class)
     override val descriptor: SerialDescriptor
-        get() = TODO("Not yet implemented")
+        get() = buildSerialDescriptor(serialName, SerialKind.ENUM) {
+            choicesNames.forEach { name ->
+                val fqn = "$serialName.${name}"
+                val enumMemberDescriptor = buildSerialDescriptor(fqn, StructureKind.OBJECT)
+                element(name, enumMemberDescriptor)
+            }
+        }
 }
-
-//// Binary backwards-compatible with plugin
-//class EnumSerializer<T : Enum<T>>(serializableClass: KClass<T>) : CommonEnumSerializer<T>(
-//    serializableClass.enumClassName(),
-//    serializableClass.enumMembers(),
-//    serializableClass.enumMembers().map { it.name }.toTypedArray()
-//)
