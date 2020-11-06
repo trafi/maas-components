1dfc7c226a81ce890eee927afd2cb458404fc4b5
Justas Medeisis <justas@trafi.com>
2020-11-06 13:58:04 +0200

    Extract common HttpClient.

--- common/core/src/commonMain/kotlin/com/trafi/routes/RoutesApi.kt
+++ common/core/src/commonMain/kotlin/com/trafi/routes/RoutesApi.kt
@@ -1,33 +1,14 @@
 package com.trafi.routes
 
 import com.trafi.core.ApiResult
+import com.trafi.core.httpClient
 import com.trafi.core.model.Location
 import com.trafi.core.model.RoutesResult
-import io.ktor.client.HttpClient
-import io.ktor.client.features.defaultRequest
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
-import io.ktor.client.request.header
 import io.ktor.client.request.parameter
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
-import kotlinx.serialization.json.Json
 
-class RoutesApi(private val baseApiUrl: String, private val apiKey: String) {
-    private val httpClient = HttpClient {
-        install(JsonFeature) {
-            val json = Json {
-                isLenient = true
-                ignoreUnknownKeys = true
-            }
-            serializer = KotlinxSerializer(json)
-        }
-        defaultRequest {
-            contentType(ContentType.Application.Json)
-            header("x-api-key", apiKey)
-        }
-    }
+class RoutesApi(private val baseApiUrl: String, apiKey: String) {
+    private val httpClient = httpClient(apiKey = apiKey)
 
     suspend fun search(start: Location, end: Location): ApiResult<RoutesResult> = try {
         val result = httpClient.get<RoutesResult>(baseApiUrl + "v1/routes") {
