b0c65fbc7ed4c866de5fc0c80a5b774a4bcb3925
Justas Medeisis <justas@trafi.com>
2020-11-12 10:26:55 +0200

    Migrate the demo app to use navigation-compose.

--- android/app/src/main/java/com/trafi/example/DemoApp.kt
+++ android/app/src/main/java/com/trafi/example/DemoApp.kt
@@ -1,42 +1,38 @@
 package com.trafi.example
 
-import androidx.compose.animation.Crossfade
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.trafi.example.ui.DemoMaasTheme
-import com.trafi.example.util.NavigationViewModel
-import com.trafi.example.util.Screen
 import com.trafi.routes.ui.RoutesScreen
-import com.trafi.ui.theme.MaasTheme
 
 @Composable
-fun DemoApp(navigationViewModel: NavigationViewModel) {
+fun DemoApp() {
     DemoMaasTheme {
-        AppContent(navigationViewModel)
+        AppContent()
     }
 }
 
 @Composable
-private fun AppContent(
-    navigationViewModel: NavigationViewModel
-) {
-    Crossfade(navigationViewModel.currentScreen) { screen ->
-        Surface(
-            color = MaasTheme.colors.background,
-            contentColor = MaasTheme.colors.onBackground
-        ) {
-            when (screen) {
-                is Screen.Onboarding -> OnboardingScreen(
-                    onComplete = { navigationViewModel.navigateTo(Screen.Routes) },
-                )
-                is Screen.Routes -> RoutesScreen(
-                    baseUrl = BuildConfig.API_BASE_URL,
-                    apiKey = BuildConfig.API_KEY,
-                    regionId = BuildConfig.REGION_ID,
-                    onRouteClick = {},
-                    onBackClick = { navigationViewModel.onBack() },
-                )
-            }
+private fun AppContent() {
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "onboarding") {
+        composable("onboarding") {
+            OnboardingScreen(
+                onComplete = { navController.navigate("routes") },
+            )
+        }
+        composable("routes") {
+            RoutesScreen(
+                baseUrl = BuildConfig.API_BASE_URL,
+                apiKey = BuildConfig.API_KEY,
+                regionId = BuildConfig.REGION_ID,
+                onRouteClick = {},
+                onBackClick = { navController.popBackStack() },
+            )
         }
     }
 }
