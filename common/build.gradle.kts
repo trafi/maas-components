a1e8dba9953596adeb673a3733107be1644b3f4b
Justas Medeisis <justas@trafi.com>
2020-11-26 10:42:05 +0200

    Update common project group and Android Gradle plugin.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,10 +7,10 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("com.android.tools.build:gradle:4.1.1")
     }
 }
-group = "com.trafi"
+group = "com.trafi.maas"
 version = "1.0-SNAPSHOT"
 
 repositories {
