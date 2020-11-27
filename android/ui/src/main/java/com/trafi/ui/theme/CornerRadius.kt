995f800b94ba6a2872df6f8c06d792c57ab074d3
Justas Medeisis <justas@trafi.com>
2020-11-27 12:41:40 +0200

    Use the common corner radius scale in Android.

--- android/ui/src/main/java/com/trafi/ui/theme/CornerRadius.kt
+++ android/ui/src/main/java/com/trafi/ui/theme/CornerRadius.kt
@@ -6,15 +6,16 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.trafi.ui.theme.internal.CornerRadiusScale
 
 object CornerRadius {
-    @Stable val none: Dp = 0.dp
-    @Stable val xxs: Dp = 4.dp
-    @Stable val xs: Dp = 6.dp
-    @Stable val sm: Dp = 8.dp
-    @Stable val lg: Dp = 12.dp
-    @Stable val xl: Dp = 20.dp
-    @Stable val round: Dp = Dp.Infinity
+    @Stable val none: Dp = CornerRadiusScale.none.dp
+    @Stable val xxs: Dp = CornerRadiusScale.xxs.dp
+    @Stable val xs: Dp = CornerRadiusScale.xs.dp
+    @Stable val sm: Dp = CornerRadiusScale.sm.dp
+    @Stable val lg: Dp = CornerRadiusScale.lg.dp
+    @Stable val xl: Dp = CornerRadiusScale.xl.dp
+    @Stable val round: Dp = CornerRadiusScale.round.dp
 }
 
 @Stable
@@ -23,5 +24,5 @@ internal val Dp.isRound: Boolean
 
 @Immutable
 data class MaasCornerRadius(
-    val buttonRadius: Dp = CornerRadius.round
+    val buttonRadius: Dp = CornerRadiusScale.Default.ButtonRadius.dp
 )
