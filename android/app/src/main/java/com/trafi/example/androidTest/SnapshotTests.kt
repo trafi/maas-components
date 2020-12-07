112d4bd6f930d82df203890454a487906766f4a5
tomasmarciulionis <tomas.marciulionis@trafi.com>
2020-12-07 13:19:45 +0200

    WIP

---
+++ android/app/src/main/java/com/trafi/example/androidTest/SnapshotTests.kt
@@ -0,0 +1,10 @@
+package com.trafi.example.androidTest
+
+import com.karumi.shot.ScreenshotTest
+import org.junit.Rule
+import org.junit.Test
+import androidx.ui.test.createComposeRule
+
+class SnapshotTests {
+
+}
