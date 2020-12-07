c0f898381b1f71256af4c10beac73f082c4bf765
tomasmarciulionis <tomas.marciulionis@trafi.com>
2020-12-07 16:53:50 +0200

    WIP

--- android/app/src/main/java/com/trafi/example/androidTest/SnapshotTests.kt
+++ android/app/src/main/java/com/trafi/example/androidTest/SnapshotTests.kt
@@ -3,6 +3,7 @@ package com.trafi.example.androidTest
 import org.junit.Rule
 import org.junit.Test
 import androidx.ui.test.createComposeRule
+import com.karumi.shot.ScreenshotTest
 
 class SnapshotTests: ScreenshotTest {
 
