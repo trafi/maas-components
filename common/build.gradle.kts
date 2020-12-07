8e6cc3f2d33c3a429a158d263d138a309fe3c417
tomasmarciulionis <tomas.marciulionis@trafi.com>
2020-12-07 13:04:09 +0200

    WIP

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("com.karumi:shot:5.4.0")
     }
 }
 group = "com.trafi.maas"
