bc059c4f6a6fa9128e4a96ceb1529ae0e6c4baad
Domas Nutautas <domas@trafi.com>
2020-12-08 13:53:47 +0200

    Implement color to Int64

--- ios/Sources/MaaSTheme/Colors.swift
+++ ios/Sources/MaaSTheme/Colors.swift
@@ -121,8 +121,42 @@ public extension Int64 {
 }
 
 public extension Color {
+
     var i64: Int64 {
-        // TODO: Implement
-        return 0
+
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+
+        uiColor().getRed(&r, green: &g, blue: &b, alpha: &a)
+        return (Int64)(a*255)<<24 | (Int64)(r*255)<<16 | (Int64)(g*255)<<8 | (Int64)(b*255)<<0
+    }
+
+    // https://stackoverflow.com/a/58531033
+    private func uiColor() -> UIColor {
+
+        if #available(iOS 14.0, *) {
+            return UIColor(self)
+        }
+
+        let components = self.components()
+        return UIColor(red: components.r, green: components.g, blue: components.b, alpha: components.a)
+    }
+
+    private func components() -> (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+
+        let scanner = Scanner(string: self.description.trimmingCharacters(in: CharacterSet.alphanumerics.inverted))
+        var hexNumber: UInt64 = 0
+        var r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
+
+        let result = scanner.scanHexInt64(&hexNumber)
+        if result {
+            r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+            g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+            b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+            a = CGFloat(hexNumber & 0x000000ff) / 255
+        }
+        return (r, g, b, a)
     }
 }
