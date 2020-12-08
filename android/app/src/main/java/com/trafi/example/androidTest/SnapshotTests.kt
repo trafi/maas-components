aa672011ad82a90ffe108a2917c25780a68311fc
tomasmarciulionis <tomas.marciulionis@trafi.com>
2020-12-08 14:42:24 +0200

    WIP

--- android/app/src/main/java/com/trafi/example/androidTest/SnapshotTests.kt
+++ android/app/src/main/java/com/trafi/example/androidTest/SnapshotTests.kt
@@ -1,14 +1,10 @@
 package com.trafi.example.androidTest
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.ui.test.createComposeRule
 import com.karumi.shot.ScreenshotTest
 import com.trafi.ui.Button
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class SnapshotTests: ScreenshotTest {
 
     @get: Rule
