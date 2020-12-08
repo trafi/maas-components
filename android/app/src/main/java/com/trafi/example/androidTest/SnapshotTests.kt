96536d20cf0cc82337b9466d61947ae86f2dc587
tomasmarciulionis <tomas.marciulionis@trafi.com>
2020-12-08 13:43:10 +0200

    WIP

--- android/app/src/main/java/com/trafi/example/androidTest/SnapshotTests.kt
+++ android/app/src/main/java/com/trafi/example/androidTest/SnapshotTests.kt
@@ -1,17 +1,28 @@
 package com.trafi.example.androidTest
 
-import org.junit.Rule
-import org.junit.Test
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.ui.test.createComposeRule
 import com.karumi.shot.ScreenshotTest
+import com.trafi.ui.Button
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class SnapshotTests: ScreenshotTest {
 
     @get: Rule
     val composeTestRule = createComposeRule()
 
     @Test
-    fun rendersStuff(){
+    fun renderButton(){
+        renderButton("test")
+        compareScreenshot(composeTestRule)
+    }
 
+    private fun renderButton(text: String){
+        composeTestRule.setContent {
+            Button(text = text, onClick = {})
+        }
     }
 }
