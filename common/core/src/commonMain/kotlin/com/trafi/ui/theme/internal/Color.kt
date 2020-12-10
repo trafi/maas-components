a62e86af4a6a61db2aece91152e7ed52e9cbb651
Justas Medeisis <justas@trafi.com>
2020-12-10 18:49:56 +0200

    Specify Color JvmName.

--- common/core/src/commonMain/kotlin/com/trafi/ui/theme/internal/Color.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/theme/internal/Color.kt
@@ -1,6 +1,9 @@
 @file:Suppress("EXPERIMENTAL_FEATURE_WARNING", "EXPERIMENTAL_API_USAGE")
+@file:JvmName("Color")
 
 package com.trafi.ui.theme.internal
+
+import kotlin.jvm.JvmName
 
 expect inline class Color(val value: ULong)
 
