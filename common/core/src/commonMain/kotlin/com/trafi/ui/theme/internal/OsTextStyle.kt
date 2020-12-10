9f536e425aab4524bdeaf8700495190c00512ffd
Justas Medeisis <justas@trafi.com>
2020-12-10 11:54:14 +0200

    Expect an OsTextStyle builder function.

--- common/core/src/commonMain/kotlin/com/trafi/ui/theme/internal/OsTextStyle.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/theme/internal/OsTextStyle.kt
@@ -2,9 +2,16 @@ package com.trafi.ui.theme.internal
 
 expect class OsTextStyle
 
-expect fun OsTextStyle.copy(
+internal expect fun OsTextStyle.copy(
     fontStyle: BasicFontStyle? = null,
     fontWeight: BasicFontWeight? = null,
     fontSize: Int? = null,
     lineHeight: Int? = null,
+): OsTextStyle
+
+internal expect fun OsTextStyle(
+    fontStyle: BasicFontStyle,
+    fontWeight: BasicFontWeight,
+    fontSize: Int,
+    lineHeight: Int,
 ): OsTextStyle
