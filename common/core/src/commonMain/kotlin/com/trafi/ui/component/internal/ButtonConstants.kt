82fb78a4f782fa47ce62cc22306060178729f314
Justas Medeisis <justas@trafi.com>
2020-12-10 11:36:33 +0200

    Simpler syntax for font weight.

--- common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
@@ -1,6 +1,6 @@
 package com.trafi.ui.component.internal
 
-import com.trafi.ui.theme.internal.BasicFontWeight
+import com.trafi.ui.theme.internal.BasicFontWeight.*
 import com.trafi.ui.theme.internal.ColorPalette.Gray200
 import com.trafi.ui.theme.internal.CurrentTheme
 import com.trafi.ui.theme.internal.copy
@@ -9,7 +9,7 @@ class ButtonConstants(theme: CurrentTheme) {
     val defaultColor = theme.colorPalette.primary
     val defaultContentColor = theme.colorPalette.onPrimary
     val disabledColor = Gray200
-    val textStyle = theme.typographyScale.textL.copy(fontWeight = BasicFontWeight.Bold)
+    val textStyle = theme.typographyScale.textL.copy(fontWeight = Bold)
     val minHeight = 48
     val cornerRadius = theme.cornerRadiusScale.buttonRadius
     val defaultTruncate = true
