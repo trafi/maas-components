f18ecfa4a3196c8cdbd7c61a0d617679a5acbbce
Justas Medeisis <justas@trafi.com>
2020-12-10 11:08:09 +0200

    Add ButtonConstants to common:core module.

---
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
@@ -0,0 +1,16 @@
+package com.trafi.ui.component.internal
+
+import com.trafi.ui.theme.internal.BasicFontWeight
+import com.trafi.ui.theme.internal.ColorPalette.Gray200
+import com.trafi.ui.theme.internal.CurrentTheme
+import com.trafi.ui.theme.internal.copy
+
+class ButtonConstants(theme: CurrentTheme) {
+    val defaultColor = theme.colorPalette.primary
+    val defaultContentColor = theme.colorPalette.onPrimary
+    val disabledColor = Gray200
+    val textStyle = theme.typographyScale.textL.copy(fontWeight = BasicFontWeight.Bold)
+    val minHeight = 48
+    val cornerRadius = theme.cornerRadiusScale.buttonRadius
+    val defaultTruncate = true
+}
