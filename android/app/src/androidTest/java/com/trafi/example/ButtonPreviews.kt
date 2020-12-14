0e737c507b07ab944367681a61c0cb41d9b17c99
tomasmarciulionis <tomas.marciulionis@trafi.com>
2020-12-14 17:16:07 +0200

    Improved tests

--- android/app/src/androidTest/java/com/trafi/example/ButtonPreviews.kt
+++ android/app/src/androidTest/java/com/trafi/example/ButtonPreviews.kt
@@ -1,29 +1,35 @@
 package com.trafi.example
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.karumi.shot.ScreenshotTest
 import com.trafi.ui.Button
 import com.trafi.ui.theme.MaasCornerRadius
 import com.trafi.ui.theme.MaasTheme
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import javax.inject.Inject
 
 class ButtonPreviews: ScreenshotTest {
-    //@get:Rule
-    //val composeTestRule = createComposeRule()
+    lateinit var previewTest: PreviewTest
+
+    @get:Rule
+    val composeTestRule: ComposeTestRule = createComposeRule()
+
+    @Before
+    fun init() {
+        previewTest = PreviewTest(composeTestRule = composeTestRule)
+    }
+
     @Test
     fun buttonTest() {
-        PreviewTest {
-            MaasTheme(cornerRadius = MaasCornerRadius(buttonRadius = 0.dp)) {
-                Button("Unlock now", onClick = {})
+        previewTest
+            .renderAndTestView {
+                MaasTheme(cornerRadius = MaasCornerRadius(buttonRadius = 0.dp)) {
+                    Button("Unlock now", onClick = {})
+                }
             }
-        }.renderAndTestView()
-
     }
 }
 
--- android/app/src/androidTest/java/com/trafi/example/PreviewTest.kt
+++ android/app/src/androidTest/java/com/trafi/example/PreviewTest.kt
@@ -2,17 +2,10 @@ package com.trafi.example
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.junit4.ComposeTestRule
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.test.platform.app.InstrumentationRegistry
 import com.karumi.shot.ScreenshotTest
-import com.trafi.example.dagger.TestComponent
-import org.junit.Rule
-import javax.inject.Inject
 
-class PreviewTest(private val composable: @Composable () -> Unit): ScreenshotTest {
-    @get: Rule
-    private val composeTestRule: ComposeTestRule = createComposeRule()
-    fun renderAndTestView() {
+class PreviewTest(private val composeTestRule: ComposeTestRule): ScreenshotTest {
+    fun renderAndTestView(composable: @Composable () -> Unit) {
         composeTestRule.setContent(composable)
         compareScreenshot(composeTestRule, "test")
     }
