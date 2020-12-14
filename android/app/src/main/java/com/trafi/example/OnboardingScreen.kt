834447eb77696a9cce9e112ffa3afaaabebe814a
Tomas Skarnulis <tomas@trafi.com>
2020-12-14 18:03:13 +0200

    moved to components; transit module;

--- android/app/src/main/java/com/trafi/example/OnboardingScreen.kt
+++ android/app/src/main/java/com/trafi/example/OnboardingScreen.kt
@@ -15,11 +15,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.trafi.example.ui.DemoMaasTheme
-import com.trafi.ui.Button
+import com.trafi.ui.components.Button
 import com.trafi.ui.theme.MaasTheme
 
 @Composable
-fun OnboardingScreen(onComplete: () -> Unit = {}) {
+fun OnboardingScreen(onComplete: () -> Unit = {}, onTransitClick: () -> Unit = {}) {
     val asset = vectorResource(id = R.drawable.onboarding_visual)
     Column(
         modifier = Modifier
@@ -40,6 +40,10 @@ fun OnboardingScreen(onComplete: () -> Unit = {}) {
         Button(
             text = "Let's go!",
             onClick = onComplete,
+        )
+        Button(
+            text = "Transit",
+            onClick = onTransitClick,
         )
     }
 }
