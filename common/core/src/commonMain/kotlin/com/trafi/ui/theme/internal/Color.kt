48ab0488f5615ec6d9361e0f5b09104717f13067
Eligijus Starinskas <eligijus@trafi.com>
2020-12-14 12:40:08 +0200

    Add dummy tint method declaration

--- common/core/src/commonMain/kotlin/com/trafi/ui/theme/internal/Color.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/theme/internal/Color.kt
@@ -11,4 +11,6 @@ import kotlin.jvm.JvmName
 
 expect inline class Color(val value: ULong)
 
+fun Color.tint(value : Float): Color = this
+
 internal fun Color(color: Long) = Color((color.toULong() and 0xffffffffUL) shl 32)
