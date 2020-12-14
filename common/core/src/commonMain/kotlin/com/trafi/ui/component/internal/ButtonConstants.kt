3e0aec84eb44daa2bcb5f5e37a438ede4a6de086
Mikas <mikas.dominas@trafi.com>
2020-12-14 18:02:04 +0200

    fix button constants to use alpha instead of tint

--- common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
@@ -2,15 +2,15 @@ package com.trafi.ui.component.internal
 
 import com.trafi.ui.theme.internal.BasicFontWeight.*
 import com.trafi.ui.theme.internal.CurrentTheme
+import com.trafi.ui.theme.internal.alpha
 import com.trafi.ui.theme.internal.copy
 import com.trafi.ui.theme.internal.dp
-import com.trafi.ui.theme.internal.tint
 
 class ButtonConstants(theme: CurrentTheme) {
     val defaultBackgroundColor = theme.colorPalette.primary
     val defaultContentColor = theme.colorPalette.onPrimary
-    val disabledBackgroundColor = theme.colorPalette.primary.tint(0.12f)
-    val disabledContentColor = theme.colorPalette.onPrimary.tint(0.38f)
+    val disabledBackgroundColor = theme.colorPalette.primary.alpha(0.12f)
+    val disabledContentColor = theme.colorPalette.onPrimary.alpha(0.38f)
     val textStyle = theme.typographyScale.textL.copy(fontWeight = Bold)
     val minHeight = 48.dp
     val cornerRadius = theme.cornerRadiusScale.buttonRadius
