17f1e4565740a5343ef3fd9f4d4189e479f278a8
Eligijus Starinskas <eligijus@trafi.com>
2020-12-14 15:18:32 +0200

    Fix compile errors

--- android/ui/src/main/java/com/trafi/ui/Button.kt
+++ android/ui/src/main/java/com/trafi/ui/Button.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.trafi.ui.theme.isRound
 import com.trafi.ui.component.internal.ButtonConstants
 import com.trafi.ui.theme.MaasCornerRadius
 import com.trafi.ui.theme.MaasTheme
