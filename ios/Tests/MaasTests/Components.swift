16cb4672354b88d05f1b10aafcb445e0a4a0fce4
Ignas Jasiūnas <ignas.jasiunas@trafi.com>
2020-12-15 14:12:38 +0200

    Snapshots and preview updates for filter

--- ios/Tests/MaasTests/Components.swift
+++ ios/Tests/MaasTests/Components.swift
@@ -3,3 +3,10 @@
 final class BadgeShots: SnapSpec<Badge_Previews> {}
 final class ButtonShots: SnapSpec<Button_Previews> {}
 final class CellShots: SnapSpec<Cell_Previews> {}
+final class SingleSelectFilterShots: SnapSpec<SingleSelectFilterPreview_Previews> {}
+final class MultiSelectFilterShots: SnapSpec<MultiSelectFilterPreview_Previews> {}
+
+// MARK: - Nearby Transit
+final class NearbyTransitFilterItemViewShots: SnapSpec<NearbyTransitFilterItemView_Previews> {}
+final class NearbyTransitFilterViewShots: SnapSpec<NearbyTransitFilterView_Previews> {}
+
