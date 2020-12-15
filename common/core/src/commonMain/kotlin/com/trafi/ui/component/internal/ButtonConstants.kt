d473776eded0405d0074a7ac0976ac093f576cf7
Eligijus Starinskas <eligijus@trafi.com>
2020-12-15 12:11:14 +0200

    Remove alpha from button disabled content

--- common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
@@ -10,7 +10,7 @@ class ButtonConstants(theme: CurrentTheme) {
     val defaultBackgroundColor = theme.colorPalette.primary
     val defaultContentColor = theme.colorPalette.onPrimary
     val disabledBackgroundColor = theme.colorPalette.primary.alpha(0.12f)
-    val disabledContentColor = theme.colorPalette.onPrimary.alpha(0.38f)
+    val disabledContentColor = theme.colorPalette.onPrimary
     val textStyle = theme.typographyScale.textL.copy(fontWeight = Bold)
     val minHeight = 48.dp
     val cornerRadius = theme.cornerRadiusScale.buttonRadius
