a285ce3a1ca3760a87cc86bce6240b64c4931efd
Justas Medeisis <justas@trafi.com>
2020-12-15 17:49:41 +0200

    explicitApi Spacing

--- android/ui/src/main/java/com/trafi/ui/theme/Spacing.kt
+++ android/ui/src/main/java/com/trafi/ui/theme/Spacing.kt
@@ -7,19 +7,19 @@ import androidx.compose.runtime.Stable
 import androidx.compose.ui.unit.Dp
 import com.trafi.ui.theme.internal.SpacingScale
 
-object Spacing {
-    @Stable val xxxs: Dp = SpacingScale.xxxs
-    @Stable val xxs: Dp = SpacingScale.xxs
-    @Stable val xs: Dp = SpacingScale.xs
-    @Stable val sm: Dp = SpacingScale.sm
-    @Stable val md: Dp = SpacingScale.md
-    @Stable val lg: Dp = SpacingScale.lg
-    @Stable val xl: Dp = SpacingScale.xl
-    @Stable val xxl: Dp = SpacingScale.xxl
-    @Stable val xxxl: Dp = SpacingScale.xxxl
+public object Spacing {
+    @Stable public val xxxs: Dp = SpacingScale.xxxs
+    @Stable public val xxs: Dp = SpacingScale.xxs
+    @Stable public val xs: Dp = SpacingScale.xs
+    @Stable public val sm: Dp = SpacingScale.sm
+    @Stable public val md: Dp = SpacingScale.md
+    @Stable public val lg: Dp = SpacingScale.lg
+    @Stable public val xl: Dp = SpacingScale.xl
+    @Stable public val xxl: Dp = SpacingScale.xxl
+    @Stable public val xxxl: Dp = SpacingScale.xxxl
 }
 
 @Immutable
-data class MaasSpacing(
+public data class MaasSpacing(
     val globalMargin: Dp = SpacingScale.Default.GlobalMargin
 )
