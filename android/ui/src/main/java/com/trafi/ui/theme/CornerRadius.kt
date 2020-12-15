1c89289604982a575fef59f4ec7a64b13d276839
Justas Medeisis <justas@trafi.com>
2020-12-15 17:49:51 +0200

    explicitApi CornerRadius

--- android/ui/src/main/java/com/trafi/ui/theme/CornerRadius.kt
+++ android/ui/src/main/java/com/trafi/ui/theme/CornerRadius.kt
@@ -7,21 +7,21 @@ import androidx.compose.runtime.Stable
 import androidx.compose.ui.unit.Dp
 import com.trafi.ui.theme.internal.CornerRadiusScale
 
-object CornerRadius {
-    @Stable val none: Dp = CornerRadiusScale.none
-    @Stable val xxs: Dp = CornerRadiusScale.xxs
-    @Stable val xs: Dp = CornerRadiusScale.xs
-    @Stable val sm: Dp = CornerRadiusScale.sm
-    @Stable val lg: Dp = CornerRadiusScale.lg
-    @Stable val xl: Dp = CornerRadiusScale.xl
-    @Stable val round: Dp = CornerRadiusScale.round
+public object CornerRadius {
+    @Stable public val none: Dp = CornerRadiusScale.none
+    @Stable public val xxs: Dp = CornerRadiusScale.xxs
+    @Stable public val xs: Dp = CornerRadiusScale.xs
+    @Stable public val sm: Dp = CornerRadiusScale.sm
+    @Stable public val lg: Dp = CornerRadiusScale.lg
+    @Stable public val xl: Dp = CornerRadiusScale.xl
+    @Stable public val round: Dp = CornerRadiusScale.round
 }
 
 @Stable
-val Dp.isRound: Boolean
+public val Dp.isRound: Boolean
     get() = this == CornerRadius.round
 
 @Immutable
-data class MaasCornerRadius(
+public data class MaasCornerRadius(
     val buttonRadius: Dp = CornerRadiusScale.Default.ButtonRadius,
 )
