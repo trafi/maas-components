36347b400ed8818ee350691961115438d54cf1aa
Eligijus Starinskas <eligijus@trafi.com>
2020-12-16 10:00:12 +0200

    Info button constants cleanup

--- common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/InfoButtonConstants.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/InfoButtonConstants.kt
@@ -5,14 +5,11 @@ import com.trafi.ui.theme.internal.BasicFontWeight.*
 import com.trafi.ui.theme.internal.copy
 
 class InfoButtonConstants(theme: CurrentTheme) {
-
+    val defaultContentColor = theme.colorPalette.onSurface
+    val disabledContentColor = defaultContentColor.alpha(0.38f)
     val textStyle = theme.typographyScale.textL.copy(fontWeight = Bold)
-    val defaultTruncate = true
-
-    val color = theme.colorPalette.grayScale.gray900
-    val disabledColor = theme.colorPalette.grayScale.gray400
+    val cornerRadius = theme.cornerRadiusScale.buttonRadius
     val highlightedColor = theme.colorPalette.grayScale.gray300
-
     val horMinPadding = SpacingScale.sm
     val verPadding = SpacingScale.sm
     val spaceBetween = SpacingScale.xs
