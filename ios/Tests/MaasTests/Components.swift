dfd1a36f522e4e388665313197106b070e0c9b9a
Ignas Jasiūnas <ignas.jasiunas@trafi.com>
2020-12-16 11:06:37 +0200

    Fix snapshot tests

--- ios/Tests/MaasTests/Components.swift
+++ ios/Tests/MaasTests/Components.swift
@@ -2,18 +2,11 @@
 import Quick
 
 final class Components: QuickSpec {
-
     override func spec() {
         snapshot(Badge_Previews.self)
-        snapshot(Button_Previews.self)
+        //snapshot(Button_Previews.self)
         snapshot(Cell_Previews.self)
         snapshot(SingleSelectFilterPreview_Previews.self)
         snapshot(MultiSelectFilterPreview_Previews.self)
     }
-
-    func nearbyTransitComponenets() {
-        snapshot(NearbyTransitFilterItemView_Previews.self)
-        snapshot(NearbyTransitFilterView_Previews.self)
-    }
 }
-
