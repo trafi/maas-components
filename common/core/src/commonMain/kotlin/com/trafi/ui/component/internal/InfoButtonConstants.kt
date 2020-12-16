637d268774bed7112c16a4553f7460835e47a6b6
Eligijus Starinskas <eligijus@trafi.com>
2020-12-16 16:25:12 +0200

    Update info button constants to match ones in design system

--- common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/InfoButtonConstants.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/InfoButtonConstants.kt
@@ -5,12 +5,27 @@ import com.trafi.ui.theme.internal.BasicFontWeight.*
 import com.trafi.ui.theme.internal.copy
 
 class InfoButtonConstants(theme: CurrentTheme) {
-    val defaultContentColor = theme.colorPalette.onSurface
-    val disabledContentColor = defaultContentColor.alpha(0.38f)
+    val iconWidth = 16.dp
+    val iconHeight = 16.dp
+
+    val paddingVertical = SpacingScale.md
+    val paddingHorizontal = SpacingScale.sm
+    val spaceBetween = SpacingScale.xs
+
+    val maxLinesCount = 1
+
     val textStyle = theme.typographyScale.textL.copy(fontWeight = Bold)
     val cornerRadius = theme.cornerRadiusScale.buttonRadius
-    val highlightedColor = theme.colorPalette.grayScale.gray300
-    val horMinPadding = SpacingScale.sm
-    val verPadding = SpacingScale.sm
-    val spaceBetween = SpacingScale.xs
+
+    val defaultContentColor = theme.colorPalette.onSurface
+    val disabledContentColor = defaultContentColor.alpha(0.38f)
+
+    val highlightedGradient = Gradient(
+        direction = Gradient.Direction.Horizontal,
+        colors = listOf(
+            theme.colorPalette.grayScale.gray300.alpha(0f),
+            theme.colorPalette.grayScale.gray300,
+            theme.colorPalette.grayScale.gray300.alpha(0f),
+        )
+    )
 }
