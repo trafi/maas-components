2536ba777b05723e5b12aa27485f4ff5289e2794
Tomas Skarnulis <tomas@trafi.com>
2020-12-16 11:32:21 +0200

    unrelated changes delete;

--- common/core/src/commonMain/kotlin/com/trafi/core/HttpClient.kt
+++ common/core/src/commonMain/kotlin/com/trafi/core/HttpClient.kt
@@ -7,14 +7,9 @@ import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
-import io.ktor.client.features.logging.*
 import kotlinx.serialization.json.Json
 
 internal fun httpClient(apiKey: String) = HttpClient {
-    install(Logging) {
-        logger = Logger.DEFAULT
-        level = LogLevel.ALL
-    }
     install(JsonFeature) {
         val json = Json {
             isLenient = true
