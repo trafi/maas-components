37abfb4072c03e585c13acbc2ad9e54dfa372543
Eligijus Starinskas <eligijus@trafi.com>
2020-12-16 11:26:29 +0200

    Remove unused imports. Change button colors mapper

--- android/ui/src/main/java/com/trafi/ui/InfoButton.kt
+++ android/ui/src/main/java/com/trafi/ui/InfoButton.kt
@@ -2,26 +2,18 @@ package com.trafi.ui
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.trafi.ui.component.internal.ButtonConstants
 import com.trafi.ui.component.internal.InfoButtonConstants
 import com.trafi.ui.theme.*
-import com.trafi.ui.theme.MaasTheme.cornerRadius
 import com.trafi.ui.theme.isRound
 
 @Composable
@@ -44,9 +36,7 @@ fun InfoButton(
         } else {
             RoundedCornerShape(constants.cornerRadius).copy()
         },
-        colors = androidx.compose.material.ButtonConstants.defaultButtonColors(
-            backgroundColor = constants.defaultBackgroundColor,
-            disabledBackgroundColor = constants.disabledBackgroundColor,
+        colors = androidx.compose.material.ButtonConstants.defaultTextButtonColors(
             contentColor = constants.defaultContentColor,
             disabledContentColor = constants.disabledContentColor,
         ),
