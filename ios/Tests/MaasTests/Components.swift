1c59f4ddc88868600e6e817977072d0007aae357
Domas Nutautas <domas@trafi.com>
2020-12-17 21:08:04 +0200

    Run tests

--- ios/Tests/MaasTests/Components.swift
+++ ios/Tests/MaasTests/Components.swift
@@ -3,8 +3,8 @@ import Quick
 
 final class Components: QuickSpec {
     override func spec() {
-        snapshot(Badge_Previews.self, precision: 0.999)
-        snapshot(Button_Previews.self, precision: 0.995)
+        snapshot(Badge_Previews.self)
+        snapshot(Button_Previews.self)
         snapshot(Cell_Previews.self)
     }
 }
