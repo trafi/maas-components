278d59b77308ae05eda836c0d14754c94198fd1a
Mikas <mikas.dominas@trafi.com>
2020-12-17 15:27:46 +0200

    Update Gradient class for better colors mapping on ios

--- common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/Gradient.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/Gradient.kt
@@ -2,9 +2,18 @@ package com.trafi.ui.component.internal
 
 import com.trafi.ui.theme.internal.Color
 
-class Gradient(
+class Gradient (
     val direction: Direction,
     val colors: List<Color>,
 ) {
+    constructor(
+        direction: Direction,
+        colors: List<ULong>,
+    ): this(direction, colors.map{ Color(it) })
+
+    val colorValues: List<ULong>
+        get() = colors.map { it.value }
+
     enum class Direction { Horizontal, Vertical }
 }
+
