1f7a57155377bdfbad531a6b69f5d84ffe42ba7e
Vytautas Stonys <vytautasstonys@ip-192-168-0-102.eu-west-1.compute.internal>
2020-12-18 12:36:18 +0200

    Implement constants into route search tab component

--- android/ui/src/main/java/com/trafi/ui/RouteSearchTab.kt
+++ android/ui/src/main/java/com/trafi/ui/RouteSearchTab.kt
@@ -1,75 +1,88 @@
 package com.trafi.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollableRow
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.AmbientContentColor
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.trafi.ui.theme.MaasTheme
-import com.trafi.ui.theme.internal.CornerRadiusScale
-import com.trafi.ui.theme.internal.SpacingScale
+import com.trafi.ui.component.internal.RouteSearchTabConstants
+import com.trafi.ui.theme.currentTheme
 
+@Composable
+private val constants
+    get() = RouteSearchTabConstants(currentTheme)
 
 data class TabItem(
     val id: String,
     val icon: String,
     val name: String,
+    val active: Boolean,
     val duration: String?,
     val price: String?,
-    val active: Boolean
 )
 
 @Composable
 fun RouteSearchTab(
     tabItem: TabItem,
     isTabSelected: Boolean,
-    onVehicleClick: () -> Unit
+    onVehicleClick: () -> Unit,
 ) {
     Card(
-        shape = RoundedCornerShape(CornerRadiusScale.xxs),
+        shape = RoundedCornerShape(constants.borderRadius),
+        border = BorderStroke(constants.borderWidth,
+            color = if (isTabSelected) constants.borderColorActive else constants.defaultBackgroundColor),
         modifier = Modifier
-            .widthIn(min = 70.dp)
-            .padding(SpacingScale.xxs)
-            .clickable(onClick = onVehicleClick)
+            .padding(constants.padding)
+            .widthIn(min = constants.minTabWidth)
+            .heightIn(min = constants.minTabHeight)
     ) {
-        Column(
-            modifier = Modifier.background(if (isTabSelected) Color.Green else Color.Blue)
+        Box(
+            modifier = Modifier
+                .clickable(onClick = onVehicleClick)
+                .background(if (isTabSelected) constants.activeBackgroundColor else constants.defaultBackgroundColor)
         ) {
-            Image(
-                vectorResource(id = R.drawable.ic_route_search_bike_s),
-                colorFilter = ColorFilter.tint(AmbientContentColor.current),
-                modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 4.dp)
-            )
-            Text(
-                tabItem.duration ?: "--",
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                style = MaasTheme.typography.textL.copy(fontWeight = FontWeight.Bold),
-            )
-            Text(
-                tabItem.price ?: "--",
-                modifier = Modifier.align(Alignment.CenterHorizontally).padding(bottom = 4.dp),
-                style = MaasTheme.typography.textM
-            )
+            Column(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(constants.padding)
+            ) {
+                Image(
+                    vectorResource(id = R.drawable.ic_route_search_bike_s),
+                    colorFilter = ColorFilter.tint(constants.contentColorPrimary),
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .width(constants.iconWidth)
+                        .height(constants.iconHeight)
+                        .padding(bottom = constants.spacerBelowIcon)
+                )
+                Text(
+                    text = tabItem.duration ?: "--",
+                    color = constants.contentColorPrimary,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(bottom = constants.spacerBelowTitle),
+                    style = constants.titleStyle,
+                )
+                Text(
+                    tabItem.price ?: "--",
+                    color = constants.contentColorSecondary,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    style = constants.subtitleStyle
+                )
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewTransportTypeSegment() {
+fun ActiveRouteSearchTabPreview() {
     RouteSearchTab(TabItem(
         id = "1",
         icon = "",
@@ -78,6 +91,45 @@ fun PreviewTransportTypeSegment() {
         price = "$15",
         active = true
     ), true) {}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RouteSearchTabPreview() {
+    RouteSearchTab(TabItem(
+        id = "1",
+        icon = "",
+        name = "Bus",
+        duration = "13min",
+        price = "$15",
+        active = true
+    ), false) {}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ActiveRouteSearchTabDarkPreview(isDarkMode: Boolean = isSystemInDarkTheme()) {
+    RouteSearchTab(TabItem(
+        id = "1",
+        icon = "",
+        name = "Bus",
+        duration = "13min",
+        price = "$15",
+        active = true
+    ), true) {}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RouteSearchTabDarkPreview() {
+    RouteSearchTab(TabItem(
+        id = "1",
+        icon = "",
+        name = "Bus",
+        duration = "13min",
+        price = "$15",
+        active = true
+    ), false) {}
 }
 
 @Preview(showBackground = true)
