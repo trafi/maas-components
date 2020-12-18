599b8c35d36abce0a2597bd22f38d52b24f4cb3f
Simas Abramovas <ab.simas@gmail.com>
2020-12-18 09:42:35 +0200

    Setup a default output path

--- common/localization/src/main/java/com/trafi/localization/Localization.kt
+++ common/localization/src/main/java/com/trafi/localization/Localization.kt
@@ -1,17 +1,23 @@
 package com.trafi.localization
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.trafi.localization.poeditor.PoEditorClient
 import kotlinx.coroutines.runBlocking
+import java.io.File
 
 fun main(args: Array<String>) = Localization().main(args)
 
 class Localization : CliktCommand() {
     private val projectId: String by option().required()
     private val apiKey: String by option().required()
-    private val outputPath: String by option().required()
+    private val outputPath: String by option().default(defaultOutputPath)
+
+    private val defaultOutputPath
+        get() = File("").absolutePath +
+                "/../core/src/commonMain/kotlin/com/trafi/core/localization"
 
     override fun run() {
         runBlocking {
