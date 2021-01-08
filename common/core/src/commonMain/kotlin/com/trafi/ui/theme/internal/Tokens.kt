2a9b5109d09147d170f85c9ace394026b64eee8c
Justas Medeisis <justas@trafi.com>
2021-01-08 18:29:40 +0200

    Update ColorPalette to use more compact light-dark Color DSL.

--- common/core/src/commonMain/kotlin/com/trafi/ui/theme/internal/Tokens.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/theme/internal/Tokens.kt
@@ -3,6 +3,7 @@ package com.trafi.ui.theme.internal
 import com.trafi.ui.theme.GrayScale
 import com.trafi.ui.theme.internal.type.BasicFontWeight
 import com.trafi.ui.theme.internal.type.Color
+import com.trafi.ui.theme.internal.type.dark
 import com.trafi.ui.theme.internal.type.dp
 
 object ColorPalette {
@@ -12,7 +13,19 @@ object ColorPalette {
     val Danger = Color(0xfff22e46)
     val Success = Color(0xff00afaf)
 
-    object DefaultLight {
+    object Default {
+        val Primary = Color(0xffff1499)
+        val PrimaryVariant = Color(0xffd20077)
+        val Secondary = Color(0xff73008b)
+        val SecondaryVariant = Color(0xff59006c)
+        val Background = White dark Black
+        val Surface = White dark Black
+        val Error = Danger
+        val OnPrimary = White
+        val OnSecondary = Black
+        val OnBackground = Color(0xff212121) dark White
+        val OnSurface = Color(0xff212121) dark White
+        val OnError = White
         val GrayScale = GrayScale(
             gray100 = Color(0xfff5f5f5),
             gray200 = Color(0xffeeeeee),
@@ -23,23 +36,7 @@ object ColorPalette {
             gray700 = Color(0xff616161),
             gray800 = Color(0xff424242),
             gray900 = Color(0xff212121),
-        )
-        val Primary = Color(0xffff1499)
-        val PrimaryVariant = Color(0xffd20077)
-        val Secondary = Color(0xff73008b)
-        val SecondaryVariant = Color(0xff59006c)
-        val Background = White
-        val Surface = White
-        val Error = Danger
-        val OnPrimary = White
-        val OnSecondary = Black
-        val OnBackground = GrayScale.gray900
-        val OnSurface = GrayScale.gray900
-        val OnError = White
-    }
-
-    object DefaultDark {
-        val GrayScale = GrayScale(
+        ) dark GrayScale(
             gray100 = Color(0xff212121),
             gray200 = Color(0xff424242),
             gray300 = Color(0xff616161),
@@ -50,18 +47,6 @@ object ColorPalette {
             gray800 = Color(0xffeeeeee),
             gray900 = Color(0xfff5f5f5),
         )
-        val Primary = Color(0xffff1499)
-        val PrimaryVariant = Color(0xffd20077)
-        val Secondary = Color(0xff73008b)
-        val SecondaryVariant = Color(0xff59006c)
-        val Background = Black
-        val Surface = Black
-        val Error = Danger
-        val OnPrimary = White
-        val OnSecondary = Black
-        val OnBackground = White
-        val OnSurface = White
-        val OnError = White
     }
 }
 
