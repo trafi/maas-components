ceaee7f04d31d4d5bc2a924ad777c1c09e207d57
Justas Medeisis <justas@trafi.com>
2021-01-19 09:48:24 +0200

    Declare more versions in the buildSrc Versions object.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,7 +7,20 @@ object Versions {
     val kotlin: String = versions.getProperty("version.kotlin")
     val androidGradlePlugin: String = versions.getProperty("version.androidGradlePlugin")
 
+    const val androidBuildToolsVersion = "30.0.3"
+    const val androidCompileSdk = 30
+    const val androidTargetSdk = 30
+    const val androidMinSdk = 23
+
     const val compose = "1.0.0-alpha08"
+
+    const val ktor = "1.4.1"
+    const val coroutines = "1.4.1-native-mt"
+    const val kotlinxSerialization = "1.0.1"
+
+    const val ktlintGradlePlugin = "9.4.1"
+    const val ktlint = "0.40.0"
+    val shot: String = versions.getProperty("version.shot")
 }
 
 private fun Any.loadProperties(propFileName: String): Properties {
