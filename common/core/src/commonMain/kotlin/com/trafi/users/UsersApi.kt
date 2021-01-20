da4f6ca1ce0024ed98168552dd9d724452241e48
Justas Medeisis <justas@trafi.com>
2021-01-20 18:18:11 +0200

    Expose createOrGetUserFlow.

--- common/core/src/commonMain/kotlin/com/trafi/users/UsersApi.kt
+++ common/core/src/commonMain/kotlin/com/trafi/users/UsersApi.kt
@@ -1,14 +1,17 @@
 package com.trafi.users
 
 import com.trafi.core.ApiResult
+import com.trafi.core.CFlow
 import com.trafi.core.Configuration
 import com.trafi.core.ConfiguredApi
+import com.trafi.core.asCFlow
 import com.trafi.core.configure
 import com.trafi.core.httpClient
 import com.trafi.core.model.Profile
 import com.trafi.core.model.UpdateProfileParameters
 import com.trafi.core.model.User
 import io.ktor.client.request.put
+import kotlinx.coroutines.flow.flow
 
 class UsersApi(configuration: Configuration): ConfiguredApi by configuration.configure {
 
@@ -22,6 +25,10 @@ class UsersApi(configuration: Configuration): ConfiguredApi by configuration.con
     } catch (e: Throwable) {
         ApiResult.Failure(e)
     }
+
+    fun createOrGetUserFlow(profile: Profile? = null): CFlow<ApiResult<User>> = flow {
+        emit(createOrGetUser(profile))
+    }.asCFlow
 
     suspend fun updateProfile(profile: Profile? = null): ApiResult<User> = try {
         val result = httpClient.authorized.put<User>(baseApiUrl + "v1/users/me/profile") {
