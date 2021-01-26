0506e99b7048f397a2eb207814227e794273aff3
Mikas <mikas.dominas@trafi.com>
2021-01-26 17:38:38 +0200

    Some examples of api usage in swift

---
+++ ios/MaasCore/Sources/MaasCore/Api/Example.swift
@@ -0,0 +1,57 @@
+import Combine
+
+private class Example1 {
+    
+    // KotlinApi wrapper class with dynamicMemberLookup convert from CFlow to Publisher
+    
+    func example() {
+        let usersApi = UsersApi(config: ApiConfiguration.config)
+        let endpointObject = usersApi.CreateOrGetUser(profile: nil)
+        let requestPublisher = KotlinApi(endpointObject).flow
+        print(requestPublisher)
+    }
+    
+    func minimisedUsage() {
+        KotlinApi(UsersApi.shared.CreateOrGetUser(profile: nil))
+            .flow
+            .sink { (completion) in
+                
+            } receiveValue: { (user) in
+                
+            }
+            .store(in: &cancelableStore)
+    }
+    
+}
+
+private class Example2 {
+    
+    // generated extension for each type of possible Response type CFlow<ApiResult<____>>
+    
+    func example() {
+        let usersApi = UsersApi(config: ApiConfiguration.config)
+        let requestPublisher = usersApi.createOrGetUser(profile: nil).publisher
+        print(requestPublisher)
+    }
+    
+    func minimisedUsage() {
+        UsersApi.shared.createOrGetUser(profile: nil)
+            .publisher
+            .sink { (completion) in
+                
+            } receiveValue: { (user) in
+                
+            }
+            .store(in: &cancelableStore)
+    }
+}
+
+
+
+// Example convenience
+private var cancelableStore = Set<AnyCancellable>()
+
+// idea to maintain configured api
+extension UsersApi {
+    static var shared = UsersApi(config: ApiConfiguration.config)
+}
