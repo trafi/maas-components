4bcaa82301f687ef0b40b2faa28ed0dbe562da0e
Justas Medeisis <justas@trafi.com>
2021-01-27 17:58:30 +0200

    Simplify iOS CFlow extensions.

--- common/core/src/iosMain/kotlin/com/trafi/users/UsersApi.kt
+++ common/core/src/iosMain/kotlin/com/trafi/users/UsersApi.kt
@@ -4,15 +4,12 @@ package com.trafi.users
 
 import com.trafi.core.ApiResult
 import com.trafi.core.CFlow
+import com.trafi.core.cFlow
 import com.trafi.core.model.Profile
 import com.trafi.core.model.User
-import com.trafi.core.wrap
-import kotlinx.coroutines.flow.flow
 
-fun UsersApi.createOrGetUser(profile: Profile? = null): CFlow<ApiResult<User>> = flow {
-    emit(createOrGetUser(profile))
-}.wrap()
+fun UsersApi.createOrGetUser(profile: Profile? = null): CFlow<ApiResult<User>> =
+    cFlow { emit(createOrGetUser(profile)) }
 
-fun UsersApi.updateProfile(profile: Profile? = null): CFlow<ApiResult<User>> = flow {
-    emit(updateProfile(profile))
-}.wrap()
+fun UsersApi.updateProfile(profile: Profile? = null): CFlow<ApiResult<User>> =
+    cFlow { emit(updateProfile(profile)) }
