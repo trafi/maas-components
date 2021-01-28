24b22ebc04dafbc029d2f2b9a206654050173eb9
Justas Medeisis <justas@trafi.com>
2021-01-28 14:17:56 +0200

    State usage from iOS

---
+++ ios/MaasCore/Sources/MaasCore/Supporting Files/AppView.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+import Combine
+import ComposableArchitecture
+
+struct AppView: View {
+
+    private let store: Store<AppState, AppAction>
+
+    init(state: AppState, environment: AppEnvironment = AppEnvironment()) {
+        store = Store(
+            initialState: state,
+            reducer: Reducer(AppState.reduce),
+            environment: environment
+        )
+    }
+
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                HStack {
+                    SwiftUI.Button("−") { viewStore.send(.DecrementButtonTapped()) }
+                    Text("\(viewStore.count)")
+                    SwiftUI.Button("+") { viewStore.send(.IncrementButtonTapped()) }
+                }
+
+                SwiftUI.Button("Number fact") { viewStore.send(.NumberFactButtonTapped()) }
+            }
+            .alert(
+                item: viewStore.binding(
+                    get: { $0.numberFactAlert.map(FactAlert.init(title:)) },
+                    send: .FactAlertDismissed()
+                ),
+                content: { Alert(title: Text($0.title)) }
+            )
+        }
+    }
+}
+
+struct FactAlert: Identifiable {
+  var title: String
+  var id: String { self.title }
+}
+
+
+#if DEBUG
+struct AppView_Previews: PreviewProvider {
+
+    static var previews: some View {
+
+        let environment = AppEnvironment()
+        environment.fetchNumberFact = { count in
+            Just(
+                AppAction.NumberFactResponse(
+                    result: ApiResultSuccess(value: "Cats! \(count)" as NSString)
+                )
+            )
+            .delay(for: 2, scheduler: RunLoop.main)
+            .eraseToEffect(cancellationId: "fetch-number-fact", cancelInFlight: true)
+        }
+
+        return AppView(state: AppState(count: 0, numberFactAlert: nil),
+                       environment: environment)
+    }
+}
+#endif
+
