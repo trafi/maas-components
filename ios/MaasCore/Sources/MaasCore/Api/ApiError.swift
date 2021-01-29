b1348d5db77fcf3084f033eb8f17aa6f7418a7d0
Justas Medeisis <justas@trafi.com>
2021-01-29 08:23:16 +0200

    Parse more specific Swift ApiError types.

--- ios/MaasCore/Sources/MaasCore/Api/ApiError.swift
+++ ios/MaasCore/Sources/MaasCore/Api/ApiError.swift
@@ -1,8 +1,20 @@
+/**
+ - Tag: ApiError
+ */
 enum ApiError: Swift.Error, Equatable, Hashable {
-    case httpError
-    case parseError
-    case error(String)
-    case unknownError
-    
-    case apiResultFailure
+    /**
+     The server returned 401.
+     - Parameter error: optional error message.
+     */
+    case unauthorized(error: Error?)
+    /**
+     The server returned an error.
+     - Parameter error: optional error message.
+     */
+    case error(error: Error?)
+    /**
+     A generic failure not covered by the more specific [ApiError](x-source-tag://ApiError) types.
+     - Parameter developerMessage: optional message to help identify the error.
+     */
+    case failure(developerMessage: String?)
 }
