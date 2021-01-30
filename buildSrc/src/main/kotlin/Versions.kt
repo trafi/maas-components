a8c9f9e9c2b09bbacb592b661d3cb240c36ec567
Justas Medeisis <justas@trafi.com>
2021-01-30 08:17:20 +0200

    ktor 1.5.1 https://ktor.io/changelog/1.5/

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -14,7 +14,7 @@ object Versions {
 
     const val compose = "1.0.0-alpha08"
 
-    const val ktor = "1.4.1"
+    const val ktor = "1.5.1"
     const val coroutines = "1.4.1-native-mt"
     const val kotlinxSerialization = "1.0.1"
 
