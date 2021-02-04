15e3733c0fb71f354e2d7d74bcd3009fac7c4ad9
Justas Medeisis <justas@trafi.com>
2021-02-04 14:36:48 +0200

    Add android-sample-convention plugin.

---
+++ buildSrc/src/main/kotlin/android-sample-convention.gradle.kts
@@ -0,0 +1,47 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdkVersion(Versions.androidCompileSdk)
+    buildToolsVersion(Versions.androidBuildToolsVersion)
+
+    defaultConfig {
+        minSdkVersion(Versions.androidMinSdk)
+        targetSdkVersion(Versions.androidTargetSdk)
+
+        versionCode = 1
+        versionName = "1.0"
+
+        buildConfigField(
+            type = "String",
+            name = "API_BASE_URL",
+            value = "\"${resolveProperty("trafi.apiBaseUrl", "TRAFI_API_BASE_URL") ?: ""}\"")
+        buildConfigField(type = "String",
+            name = "API_KEY",
+            value = "\"${resolveProperty("trafi.apiKey", "TRAFI_API_KEY") ?: ""}\"")
+        buildConfigField(
+            type = "String",
+            name = "REGION_ID",
+            value = "\"${resolveProperty("trafi.regionId", "TRAFI_REGION_ID") ?: ""}\"")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        useIR = true
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerVersion = Versions.kotlin
+        kotlinCompilerExtensionVersion = Versions.compose
+    }
+}
