5e257d481cc3a5ac4a10cdf90289b78248b72f04
Mikas <mikas.dominas@trafi.com>
2021-02-05 09:55:26 +0200

    new buttton constants combining buttonConstants and infoButtonConstants

--- common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
@@ -1,6 +1,8 @@
 package com.trafi.ui.component.internal
 
 import com.trafi.ui.theme.internal.CurrentTheme
+import com.trafi.ui.theme.internal.SpacingScale
+import com.trafi.ui.theme.internal.maxValue
 import com.trafi.ui.theme.internal.type.BasicFontWeight.Bold
 import com.trafi.ui.theme.internal.type.alpha
 import com.trafi.ui.theme.internal.type.copy
@@ -15,4 +17,30 @@ class ButtonConstants(theme: CurrentTheme) {
     val minHeight = 48.dp
     val cornerRadius = theme.cornerRadiusScale.buttonRadius
     val defaultTruncate = true
+}
+
+open class NewButtonConstants(theme: CurrentTheme) {
+
+    val defaultBackgroundColor = theme.colorPalette.primary
+    val defaultContentColor = theme.colorPalette.onPrimary
+    val disabledBackgroundColor = theme.colorPalette.primary.alpha(0.12f)
+    val disabledContentColor = theme.colorPalette.onPrimary
+
+    val iconWidth = 16.dp
+    val iconHeight = 16.dp
+
+    open val textStyle = theme.typographyScale.textL.copy(fontWeight = Bold)
+    val defaultTruncate = true
+
+    open val minHeight = 48.dp
+
+    val spaceBetween = SpacingScale.xs
+    val paddingHorizontal = SpacingScale.md
+
+    val cornerRadius = theme.cornerRadiusScale.buttonRadius
+}
+
+class CompactButtonConstants(theme: CurrentTheme) : NewButtonConstants(theme) {
+    override val textStyle = theme.typographyScale.textM.copy(fontWeight = Bold)
+    override val minHeight = 32.dp
 }
