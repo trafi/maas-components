06d454ca35a5363a1b03960f8dafde2323f69b7d
Mikas <mikas.dominas@trafi.com>
2021-02-07 18:58:54 +0200

    Fix rename Error -> PlatformError in swift

--- ios/MaasCore/Sources/MaasCore/Api/ApiError.swift
+++ ios/MaasCore/Sources/MaasCore/Api/ApiError.swift
@@ -6,12 +6,12 @@ enum ApiError: Swift.Error, Equatable, Hashable {
      The server returned 401.
      - Parameter error: optional error message.
      */
-    case unauthorized(error: Error?)
+    case unauthorized(error: PlatformError?)
     /**
      The server returned an error.
      - Parameter error: optional error message.
      */
-    case error(error: Error?)
+    case error(error: PlatformError?)
     /**
      A generic failure not covered by the more specific [ApiError](x-source-tag://ApiError) types.
      - Parameter developerMessage: optional message to help identify the error.
