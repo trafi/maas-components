ef1408027f3e04320e1f031af0761ed7f9fd4072
Justas Medeisis <justas@trafi.com>
2021-02-08 16:15:50 +0200

    Wire up the Firebase auth sample app.

--- android/samples/auth-firebase/src/main/java/com/trafi/sample/FirebaseAuthSampleApp.kt
+++ android/samples/auth-firebase/src/main/java/com/trafi/sample/FirebaseAuthSampleApp.kt
@@ -1,7 +1,38 @@
 package com.trafi.sample
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.flow.collect
 
 @Composable
-fun FirebaseAuthSampleApp() {
+fun FirebaseAuthSampleApp(
+    viewModel: SampleViewModel,
+    onContinueWithGoogleClick: () -> Unit,
+) {
+    val user by viewModel.user.collectAsState()
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "welcome") {
+        composable("welcome") {
+            WelcomeScreen(
+                onLaterClick = { navController.popBackStack() },
+                onContinueWithGoogleClick = onContinueWithGoogleClick,
+            )
+        }
+        composable("profile") {
+            ProfileScreen(user?.profile)
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.signInComplete.collect { complete ->
+            if (complete) navController.navigate("profile")
+        }
+    }
 }
