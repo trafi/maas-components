b5525097f545e6111ba774461f01d4c9fd7c1098
Mikas <mikas.dominas@trafi.com>
2021-02-08 14:07:07 +0200

    Replace ButtonConstants with NewButtonConstants

--- common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
+++ common/core/src/commonMain/kotlin/com/trafi/ui/component/internal/ButtonConstants.kt
@@ -8,17 +8,6 @@ import com.trafi.ui.theme.internal.type.copy
 import com.trafi.ui.theme.internal.type.dp
 
 class ButtonConstants(theme: CurrentTheme) {
-    val defaultBackgroundColor = theme.colorPalette.primary
-    val defaultContentColor = theme.colorPalette.onPrimary
-    val disabledBackgroundColor = theme.colorPalette.primary.alpha(0.12f)
-    val disabledContentColor = theme.colorPalette.onPrimary
-    val textStyle = theme.typographyScale.textL.copy(fontWeight = Bold)
-    val minHeight = 48.dp
-    val cornerRadius = theme.cornerRadiusScale.buttonRadius
-    val defaultTruncate = true
-}
-
-class NewButtonConstants(theme: CurrentTheme) {
 
     val defaultBackgroundColor = theme.colorPalette.primary
     val defaultContentColor = theme.colorPalette.onPrimary
