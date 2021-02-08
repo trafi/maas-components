d146fb3c9cfed63648b48b4b9d439e701481d4a9
Justas Medeisis <justas@trafi.com>
2021-02-08 16:15:50 +0200

    Wire up the Firebase auth sample app.

--- android/samples/auth-firebase/src/main/java/com/trafi/sample/MainActivity.kt
+++ android/samples/auth-firebase/src/main/java/com/trafi/sample/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.trafi.sample
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.setContent
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: SampleViewModel by viewModels()
+    private val googleIdp = GoogleIdentityProvider(
+        activity = this,
+        onSignInSuccess = { viewModel.onSignInSuccess(it) },
+        onSignInError = { viewModel.onSignInError(it) },
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { FirebaseAuthSampleApp() }
+        setContent {
+            FirebaseAuthSampleApp(
+                viewModel = viewModel,
+                onContinueWithGoogleClick = { googleIdp.signIn() },
+            )
+        }
     }
 }
