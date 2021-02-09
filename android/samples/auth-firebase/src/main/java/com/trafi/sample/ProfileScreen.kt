0b0f76e130a611fa21371a9778ea2bdf7fbf8d2a
Justas Medeisis <justas@trafi.com>
2021-02-09 09:56:29 +0200

    Add ProfileScreen callbacks to update profile, log out and corrupt the token.

--- android/samples/auth-firebase/src/main/java/com/trafi/sample/ProfileScreen.kt
+++ android/samples/auth-firebase/src/main/java/com/trafi/sample/ProfileScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -24,12 +25,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.trafi.core.model.Profile
+import com.trafi.ui.component.Button
 import com.trafi.ui.theme.MaasTheme
 import com.trafi.ui.theme.Spacing
 import com.trafi.ui.theme.internal.CornerRadiusScale
 
 @Composable
-fun ProfileScreen(profile: Profile? = null) {
+fun ProfileScreen(
+    profile: Profile?,
+    onUpdateProfileClick: (Profile) -> Unit,
+    onLogOutClick: () -> Unit,
+    onCorruptTokenClick: () -> Unit,
+) {
     var name by remember(profile) { mutableStateOf(profile?.firstName ?: "John") }
     var surname by remember(profile) { mutableStateOf(profile?.lastName ?: "Smith") }
 
@@ -61,6 +68,23 @@ fun ProfileScreen(profile: Profile? = null) {
             label = "Surname",
             modifier = Modifier.fillMaxWidth(),
         )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        TertiaryButton(text = "Update", onClick = {
+            onUpdateProfileClick(Profile(
+                gender = profile?.gender ?: Profile.Gender.NOT_SPECIFIED,
+                ext = profile?.ext.orEmpty(),
+                firstName = name,
+                lastName = surname,
+                displayName = profile?.displayName,
+                email = profile?.email,
+                address = profile?.address,
+                birthDate = profile?.birthDate,
+            ))
+        })
+        TertiaryButton(text = "Log out", onClick = onLogOutClick)
+        TertiaryButton(text = "Corrupt token", onClick = onCorruptTokenClick)
     }
 }
 
@@ -103,8 +127,28 @@ private fun Input(
     )
 }
 
+@Composable
+private fun TertiaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        text = text,
+        onClick = onClick,
+        color = MaasTheme.colors.background,
+        contentColor = MaasTheme.colors.onBackground,
+        modifier = modifier,
+    )
+}
+
 @Preview
 @Composable
 private fun ProfileScreenPreview() {
-    ProfileScreen()
+    ProfileScreen(
+        profile = null,
+        onUpdateProfileClick = {},
+        onLogOutClick = {},
+        onCorruptTokenClick = {},
+    )
 }
