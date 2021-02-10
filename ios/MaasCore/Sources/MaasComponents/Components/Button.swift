fe2e22c7eb242f107951559b69c2b0779b58147e
Mikas <mikas.dominas@trafi.com>
2021-02-10 11:41:44 +0200

    Remove ExampleButton.Swift, and keep it under #Debug in Button.swift

--- ios/MaasCore/Sources/MaasComponents/Components/Button.swift
+++ ios/MaasCore/Sources/MaasComponents/Components/Button.swift
@@ -169,4 +169,64 @@ public struct Button_Previews: PreviewProvider, Snapped {
     public static var detailed: Bool { true }
     public static var elementWidth: CGFloat? { 320 }
 }
+
+struct ExampleButton: View {
+    
+    @State private var isLoadingButton = false
+    @State var dFlag = false
+    
+    var title: String {
+        dFlag
+            ? isLoadingButton
+            ? ""
+            : "Button"
+            :  isLoadingButton
+            ? "Iconning..."
+            : "ICON"
+    }
+    
+    var icon: Image? {
+        dFlag
+            ? Image(systemName: "command")
+            : nil
+    }
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            HStack {
+                SwiftUI.Button("start") { isLoadingButton = true }
+                SwiftUI.Button("stop") { isLoadingButton = false }
+                SwiftUI.Button("icon") {
+                    dFlag.toggle()
+                }
+            }
+            
+            Button(
+                title,
+                icon: icon,
+                isHugging: false,
+                isLoading: $isLoadingButton,
+                action: tappedButton
+            ).padding()
+            
+            Spinner(color: .black, lineWidth: 20, isAnimating: $isLoadingButton)
+                .frame(width: 100, height: 100, alignment: .center)
+        }
+    }
+    
+    
+    func tappedButton() {
+        isLoadingButton = true
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+            isLoadingButton = false
+        }
+    }
+}
+
+struct ExampleButton_Preview: PreviewProvider {
+    
+    static var previews: some View {
+        ExampleButton()
+    }
+}
 #endif
