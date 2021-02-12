1381082d45bc3b94be7863ce3d59345ac0bec386
Mikas <mikas.dominas@trafi.com>
2021-02-12 17:08:30 +0200

    RetryOn publisher with more customisation

--- ios/MaasCore/Sources/MaasCore/Extensions/RetryOn.swift
+++ ios/MaasCore/Sources/MaasCore/Extensions/RetryOn.swift
@@ -3,8 +3,10 @@ import Combine
 // https://www.wwt.com/article/creating-your-own-custom-combine-operator
 
 extension Publishers {
+
     /// A publisher that attempts to recreate its subscription to a failed upstream publisher.
-    public struct RetryOn<Upstream: Publisher, ErrorType: Swift.Error & Equatable>: Publisher {
+    @available(iOS 14.0, *)
+    public struct RetryOn<Upstream: Publisher, ChainedPublisher: Publisher>: Publisher {
         
         public typealias Output = Upstream.Output
         
@@ -12,44 +14,39 @@ extension Publishers {
         
         let upstream: Upstream
         let retries: UInt
-        let error: ErrorType
-        let chainedPublisher: AnyPublisher<Output, Failure>?
+        let failureShouldRetry: (Failure) -> Bool
+        let chainedPublisher: ChainedPublisher?
+        let chainOnEveryError: Bool
         
-        /// Creates a publisher that attempts to recreate its subscription to a failed upstream publisher.
-        ///
-        /// - Parameters:
-        ///   - upstream: The publisher from which this publisher receives its elements.
-        ///   - error: An equatable error that should trigger the retry
-        ///   - retries: The number of times to attempt to recreate the subscription.
-        ///   - chainedPublisher: An optional publisher of the same type, to chain before the retry
-        public init(upstream: Upstream, retries: UInt, error: ErrorType, chainedPublisher: AnyPublisher<Output, Failure>?) {
+        public init(upstream: Upstream, retries: UInt, failureShouldRetry: @escaping (Failure) -> Bool, chainedPublisher: ChainedPublisher?, chainOnEveryError: Bool = true) {
             self.upstream = upstream
             self.retries = retries
-            self.error = error
+            self.failureShouldRetry = failureShouldRetry
             self.chainedPublisher = chainedPublisher
+            self.chainOnEveryError = chainOnEveryError
         }
         
         public func receive<S: Subscriber>(subscriber: S) where Upstream.Failure == S.Failure, Upstream.Output == S.Input {
-            upstream
+            self.upstream
                 .catch { e -> AnyPublisher<Output, Failure> in
-                    guard (e as? ErrorType) == self.error,
-                          self.retries > 0 else {
-                        //if it is not the specific error we should retry on, just pass it forward in the stream
+                    guard failureShouldRetry(e), retries > 0 else {
                         return Fail<Output, Failure>(error: e)
                             .eraseToAnyPublisher()
                     }
-                    if let chainedPublisher = self.chainedPublisher {
-                        //if we have a chained publisher, use it before retrying
-                        return chainedPublisher
-                            .flatMap { value -> AnyPublisher<Output, Failure> in
-                                self.upstream
-                                    .retryOn(self.error, retries:self.retries - 1)
-                                    .eraseToAnyPublisher()
-                            }.eraseToAnyPublisher()
-                    }
-                    return self.upstream
-                        .retryOn(self.error, retries:self.retries - 1)
+                    
+                    let retriedUpstream = upstream
+                        .retryOn(failureShouldRetry: failureShouldRetry,
+                                 retries: retries - 1,
+                                 chainedPublisher: chainOnEveryError ? chainedPublisher : nil)
                         .eraseToAnyPublisher()
+                    
+                    if let chainedPublisher = chainedPublisher {
+                        return chainedPublisher
+                            .catch { _ in Fail(error: e) }
+                            .flatMap { _ in retriedUpstream }
+                            .eraseToAnyPublisher()
+                    }
+                    return retriedUpstream
                 }
                 .subscribe(subscriber)
         }
@@ -57,22 +54,19 @@ extension Publishers {
 }
 
 extension Publisher {
-    
-    /// Attempts to recreate a failed subscription with the upstream publisher using a specified number of attempts to establish the connection.
-    ///
-    /// After exceeding the specified number of retries, the publisher passes the failure to the downstream receiver.
-    /// - Parameter error: An equatable error that should trigger the retry
-    /// - Parameter retries: The number of times to attempt to recreate the subscription.
-    /// - Parameter chainedPublisher: An optional publisher of the same type, to chain before the retry
-    /// - Returns: A publisher that attempts to recreate its subscription to a failed upstream publisher.
-    public func retryOn<E: Swift.Error & Equatable>(
-        _ error: E,
+
+    @available(iOS 14.0, *)
+    public func retryOn<ChainedPublisher: Publisher>(
+        failureShouldRetry: @escaping (Failure) -> Bool,
         retries: UInt,
-        chainedPublisher: AnyPublisher<Output, Failure>? = nil
-    ) -> Publishers.RetryOn<Self, E> {
+        chainedPublisher: ChainedPublisher? = nil,
+        chainOnEveryError: Bool = true
+    ) -> Publishers.RetryOn<Self, ChainedPublisher> {
         .init(upstream: self,
               retries: retries,
-              error: error,
-              chainedPublisher: chainedPublisher)
+              failureShouldRetry: failureShouldRetry,
+              chainedPublisher: chainedPublisher,
+              chainOnEveryError: chainOnEveryError
+        )
     }
 }
