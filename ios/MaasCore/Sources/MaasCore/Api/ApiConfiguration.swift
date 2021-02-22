c9a1efd44c874fa8052f9715ec7843b31545f29f
Ignas Jasiūnas <ignas.jasiunas@trafi.com>
2021-02-22 12:29:54 +0200

    Add support for Maas-Info.plist

--- ios/MaasCore/Sources/MaasCore/Api/ApiConfiguration.swift
+++ ios/MaasCore/Sources/MaasCore/Api/ApiConfiguration.swift
@@ -28,4 +28,50 @@ public extension ApiConfig {
             logger: logger
         )
     }
+
+    var apiKey: String { MaasEnvironment.shared.apiKey }
+
+    var baseUrl: String { MaasEnvironment.shared.baseUrl }
+}
+
+// MARK: - Maas Environment
+
+private struct MaasEnvironment {
+
+    static let shared = MaasEnvironment()
+
+    let config: NSDictionary
+
+    init() {
+
+        guard
+            let configPath = Bundle.main.path(forResource: "Maas-Info", ofType: "plist"),
+            let config = NSDictionary(contentsOfFile: configPath)
+        else {
+            fatalError("Maas `ApiConfig` could not find a valid Maas-Info.plist in your project. Please add one or define baseUrl and api key manualy.")
+        }
+
+        self.config = config
+    }
+}
+
+extension MaasEnvironment {
+
+    var baseUrl: String {
+        guard let baseUrl = getValue(key: "MAAS_BASE_URL") else {
+            fatalError("Could not find `MAAS_BASE_URL` inside Maas-Info.plist")
+        }
+        return baseUrl
+    }
+
+    var apiKey: String {
+        guard let baseUrl = getValue(key: "MAAS_API_KEY") else {
+            fatalError("Could not find `MAAS_API_KEY` inside Maas-Info.plist")
+        }
+        return baseUrl
+    }
+
+    func getValue(key: String) -> String? {
+        config[key] as? String ?? ProcessInfo.processInfo.environment[key]
+    }
 }
