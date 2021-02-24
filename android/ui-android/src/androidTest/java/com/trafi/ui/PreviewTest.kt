33c2ede9e5ffa56b77f8c3ee8be4bab43594880e
Justas Medeisis <justas@trafi.com>
2021-02-24 22:18:10 +0200

    Move karumo/shot screenshot tests from the sample app to the ui-android library module.

--- android/ui-android/src/androidTest/java/com/trafi/ui/PreviewTest.kt
+++ android/ui-android/src/androidTest/java/com/trafi/ui/PreviewTest.kt
@@ -1,10 +1,10 @@
-package com.trafi.example
+package com.trafi.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import com.karumi.shot.ScreenshotTest
 
-class PreviewTest(private val composeTestRule: ComposeContentTestRule) : ScreenshotTest {
+internal class PreviewTest(private val composeTestRule: ComposeContentTestRule) : ScreenshotTest {
     fun renderAndTestView(screenshotName: String, composable: @Composable () -> Unit) {
         composeTestRule.setContent(composable)
         compareScreenshot(composeTestRule, screenshotName)
