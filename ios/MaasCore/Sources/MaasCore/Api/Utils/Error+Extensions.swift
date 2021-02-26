84232b77e1df2fb85dd79ca4304d3b7a01c3b043
Mikas <mikas.dominas@trafi.com>
2021-02-26 09:40:51 +0200

    reorder parameters for preSinkError

--- ios/MaasCore/Sources/MaasCore/Api/Utils/Error+Extensions.swift
+++ ios/MaasCore/Sources/MaasCore/Api/Utils/Error+Extensions.swift
@@ -58,13 +58,13 @@ extension Publisher where Failure == ApiError {
     /// Assigns error from a publisher to a property on an object
     /// - Parameters:
     ///   - completeImmediately: A Boolean value that indicates whether the publisher should immediately finish.
-    ///   - keyPath: A key path that indicates the property to assign
     ///   - root: The object that contains the property. The subscriber assigns the object’s property when error is received.
+    ///   - keyPath: A key path that indicates the property to assign
     /// - Returns: A publisher that never publishes failure.
     public func preSinkError<Root>(
         completeImmediately: Bool = true,
-        to keyPath: ReferenceWritableKeyPath<Root, ApiError>,
-        on root: Root
+        on root: Root,
+        to keyPath: ReferenceWritableKeyPath<Root, ApiError>
     ) -> AnyPublisher<Self.Output, Never> {
         preSinkError(completeImmediately: completeImmediately) { root[keyPath: keyPath] = $0 }
     }
@@ -72,13 +72,13 @@ extension Publisher where Failure == ApiError {
     /// Assigns error from a publisher to a property on an object
     /// - Parameters:
     ///   - completeImmediately: A Boolean value that indicates whether the publisher should immediately finish.
-    ///   - keyPath: A key path that indicates the property to assign
     ///   - root: The object that contains the property. The subscriber assigns the object’s property when error is received.
+    ///   - keyPath: A key path that indicates the property to assign
     /// - Returns: A publisher that never publishes failure.
     public func preSinkError<Root>(
         completeImmediately: Bool = true,
-        to keyPath: ReferenceWritableKeyPath<Root, ApiError?>,
-        on root: Root
+        on root: Root,
+        to keyPath: ReferenceWritableKeyPath<Root, ApiError?>
     ) -> AnyPublisher<Self.Output, Never> {
         preSinkError(completeImmediately: completeImmediately) { root[keyPath: keyPath] = $0 }
     }
