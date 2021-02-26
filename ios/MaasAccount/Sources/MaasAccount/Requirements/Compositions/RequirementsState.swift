773d87c19ab369dd90758673ca66c1928cb645b3
Ignas Jasiūnas <ignas.jasiunas@trafi.com>
2021-02-26 10:32:35 +0200

    State publishers in one place.

--- ios/MaasAccount/Sources/MaasAccount/Requirements/Compositions/RequirementsState.swift
+++ ios/MaasAccount/Sources/MaasAccount/Requirements/Compositions/RequirementsState.swift
@@ -4,10 +4,9 @@ import MaasCore
 public class RequirementsState: ObservableObject {
 
     @Published public var providerRequirements: VerifyProviderRequirementsResponse
+    @Published public var displayRequirementsList: Bool = false
 
     public init(providerRequirements: VerifyProviderRequirementsResponse) {
         self.providerRequirements = providerRequirements
     }
-
-    @Published public var displayRequirementsList: Bool = false
 }
