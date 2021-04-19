0aa10e5046ffd9a1dc0ad9c48cad97370774675e
Klaidas Strazdauskas <klaidas@trafi.com>
2021-04-19 12:58:14 +0300

    undo last change

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Maas",
-            targets: ["Maas", "Examples", "CoreBinary"]),
+            targets: ["Maas", "Examples"]),
     ] + environment.products,
     dependencies: [
         // Dependencies declare other packages that this package depends on.
